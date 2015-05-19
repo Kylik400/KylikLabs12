de5a15271ff95348a7a3a07d68041137cddd1bb4
Alex Bajmutov <bajmutov@gmail.com>
2015-05-19 21:09:38 +0300

    Lab#2

--- Labs/src/Main.java
+++ Labs/src/Main.java
@@ -4,32 +4,51 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int s[] = {0,1,2,3,4,5,6,7,8,9,};
+        int s[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+        int counter = 0;
 
-        for(int i = 0; i <=10 ;i++) {
-            int g = 0;
+        for (int i = 0; i < 10; i++) {
 
-            s[i] = g;
 
-            if (g <= 0) {
+            if (s[i] == 0) {
+                counter++;
 
-                System.out.println(g);
+                System.out.println(s[i]);
+
             }
+
         }
+        System.out.println(counter);
+
 
         //Второе задание
 
-        int[] q = {2,5,6,7,8,9,1};
+        int q[] = {2, 5, 6, 7, 8};
 
-        for(int e; e)
+        int t = 0;
+
+
+        for (int i = 0; i <= s.length; i++) {
+
+            int max = q[0];
+            t = q[i];
+
+
+            if (t >= max) {
+
+                max = t;
+               
+
+
+            }
+
+        }
 
 
 
     }
-
-
-    }
-
-
-
 }
+
+
+
+
