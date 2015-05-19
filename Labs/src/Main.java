14692f25ff4f61d2e89f2152e5355c34474bbb03
Alex Bajmutov <bajmutov@gmail.com>
2015-05-19 20:17:15 +0300

    Initial commit

---
+++ Labs/src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int s[] = {0,1,2,3,4,5,6,7,8,9,};
+
+        for(int i = 0; i <=10 ;i++) {
+            int g = 0;
+
+            s[i] = g;
+
+            if (g <= 0) {
+
+                System.out.println(g);
+            }
+        }
+
+        //Второе задание
+
+        int[] q = {2,5,6,7,8,9,1};
+
+        for(int e; e)
+
+
+
+    }
+
+
+    }
+
+
+
+}
