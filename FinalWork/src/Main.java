adcb934230c2ce9e254613aa0aafcf46f8806c1f
kylik400 <kylik400@mail.ru>
2015-08-04 18:29:54 +0400

    123 44

--- FinalWork/src/Main.java
+++ FinalWork/src/Main.java
@@ -1,4 +1,10 @@
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by Dimon on 03.08.2015.
@@ -8,5 +14,29 @@ public class Main {
 
         ClassInput tr = new ClassInput();
 
+
+        URL url = new URL("http://cashexchange.com.ua/XmlApi.ashx");
+        InputStream in = new BufferedInputStream(url.openStream());
+
+        Document doc = parseXML(in);
+        System.out.println(doc);
+
+
+    private static Document parseXML(InputStream stream) throws Exception {
+        DocumentBuilderFactory objDocumentBuilderFactory = null;
+        DocumentBuilder objDocumentBuilder = null;
+        Document doc = null;
+        try {
+            objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+            objDocumentBuilder = objDocumentBuilderFactory.newDocumentBuilder();
+            doc = objDocumentBuilder.parse(stream);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return doc;
     }
+
+}
+
 }
