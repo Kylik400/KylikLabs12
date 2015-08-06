437c9ad29367631866ac6e8d852dc7e83e17e2b6
kylik400 <kylik400@mail.ru>
2015-08-06 20:26:18 +0400

    123 44

--- FinalWork/src/IncorrectName.java
+++ FinalWork/src/IncorrectName.java
@@ -1,9 +1,31 @@
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+
 /**
  * Created by Dimon on 04.08.2015.
  */
 public class IncorrectName extends Throwable {
 
 
+   /* Document doc = parseXML(in);
+    System.out.println(doc);
 
+
+    private static Document parseXML(InputStream stream) throws Exception {
+        DocumentBuilderFactory objDocumentBuilderFactory = null;
+        DocumentBuilder objDocumentBuilder = null;
+        Document doc = null;
+        try {
+            objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+            objDocumentBuilder = objDocumentBuilderFactory.newDocumentBuilder();
+            doc = objDocumentBuilder.parse(stream);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return doc;
+    }*/
 
 }
--- FinalWork/src/Main.java
+++ FinalWork/src/Main.java
@@ -1,41 +1,56 @@
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-/**
- * Created by Dimon on 03.08.2015.
- */
-public class Main {
-    public static void main(String[] args) throws IOException {
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
 
-        ClassInput tr = new ClassInput();
+
+public class Main {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
 
         URL url = new URL("http://cashexchange.com.ua/XmlApi.ashx");
         InputStream in = new BufferedInputStream(url.openStream());
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+        Document doc = (Document) documentBuilder.parse(in);
 
-        Document doc = parseXML(in);
-        System.out.println(doc);
+        String title = doc.getDocumentElement().getNodeName();
+        System.out.println(title);
+
+        NodeList nodeList = doc.getElementsByTagName("Results").item(0).getChildNodes();
+
+        String currency = "";
+        double buy = 0;
+        double sell = 0;
+
+        for (int i = 0; i < nodeList.getLength(); i++){
+            Element element = (Element) nodeList.item(i);
+
+                currency = element.getElementsByTagName("Currency").item(0).getChildNodes().item(0).getNodeValue();
+               buy = Double.parseDouble(element.getElementsByTagName("Buy").item(0).getChildNodes().item(0).getNodeValue());
+                sell = Double.parseDouble(element.getElementsByTagName("Sell").item(0).getChildNodes().item(0).getNodeValue());
+
+                System.out.println(currency +  " " + buy + " " + sell );
 
 
-    private static Document parseXML(InputStream stream) throws Exception {
-        DocumentBuilderFactory objDocumentBuilderFactory = null;
-        DocumentBuilder objDocumentBuilder = null;
-        Document doc = null;
-        try {
-            objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
-            objDocumentBuilder = objDocumentBuilderFactory.newDocumentBuilder();
-            doc = objDocumentBuilder.parse(stream);
-        } catch (Exception ex) {
-            throw ex;
+
+
         }
 
-        return doc;
-    }
+
+
+
+
+
 
 }
 
